9451ddb2fba7e2c73f44bb703d7babe802bb876d
jonat <jonat@JP-TABLET.dhcp.wustl.edu>
2022-10-13 12:46:01 -0500

    studio4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,12 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(128,64,0);
+		StdDraw.filledRectangle(0.5, 0.25, 0.49, 0.24);
+		StdDraw.setPenColor(0,128,0);
+		StdDraw.filledRectangle(0.5, 0.75, 0.49, 0.24);
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.rectangle(0.5, 0.5, 0.49, 0.49);
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,19 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+		int r = in.nextInt();
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean isfilled = in.nextBoolean();
+		double p1 = in.nextDouble();
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		StdDraw.setPenColor(r, g, b);
+		if (shape=="ellipse")
+			{
+			StdDraw.ellipse(p1, p2, p3, p4);
+			}
 	}
 }
